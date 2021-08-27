aacf80da45490cf244ad6ab646730762d3c023ac
Samet Budak 0291000349 <samet.budak@netaszm.com.tr>
2021-08-27 19:08:02 +0300

    double game added with tests

--- src/main/java/com/workshops/javasamples/javacodechallenges/LinkedinCourseExercises.java
+++ src/main/java/com/workshops/javasamples/javacodechallenges/LinkedinCourseExercises.java
@@ -1,6 +1,16 @@
 package com.workshops.javasamples.javacodechallenges;
 
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
 public class LinkedinCourseExercises {
+
+    public static int startPoints = 10;
+    private Scanner scanner = new Scanner(System.in);
+    private Random random = new Random();
+    private int currentPoint = startPoints;
+    private boolean isStillPlaying = true;
 
     public static boolean isEven(int number) {
         return number % 2 == 0;
@@ -59,6 +69,74 @@ public class LinkedinCourseExercises {
                 password.matches(".*[A-Z].*");
 
 
+    }
+
+    /**
+     * Double or Nothing
+     * User starts with 10 points
+     * User can take a chance to Double their points
+     * if user takes the chance , randomness decides if they recieve double or loses all
+     * @return
+     */
+    public void playGame() {
+        printWelcome();
+        askUserToContinue();
+
+        while (this.isStillPlaying) {
+            printTryToDouble();
+
+            if(shouldDouble()) {
+                doubleCurrent();
+                printCongrats();
+                askUserToContinue();
+
+            } else {
+                printLost();
+                return;
+            }
+
+            pritnWalkawayMessage();
+        }
+    }
+
+    public void doubleCurrent() {
+        this.currentPoint = this.currentPoint * 2;
+    }
+
+    public boolean shouldDouble() {
+        return this.random.nextInt(2) == 0;
+    }
+
+    public void printLost() {
+        System.out.println("You have lost. No points for you");
+    }
+
+    public void pritnWalkawayMessage() {
+        System.out.println("Congrats! You finish with " + this.currentPoint + " points. \n");
+    }
+
+    public void printCongrats() {
+        System.out.println("Congrats! You now have " + this.currentPoint + " points. \n");
+    }
+
+    public void printTryToDouble() {
+        System.out.println("Let's try to double it");
+    }
+
+    protected void askUserToContinue() {
+        System.out.println("Would you like to continue " +
+                "and try to double your winnings? " +
+                "Enter YES to continue");
+        this.isStillPlaying = this.scanner.nextLine()
+                .toUpperCase().equals("YES");
+    }
+
+    public void printWelcome() {
+        System.out.println("Welcome to the Double or not Game. Your start point is : " + startPoints);
+    }
+
+    protected boolean getIsStillPlaying() {
+        return this.isStillPlaying;
     }
 
     public static void main(String[] args) {
--- src/test/java/com/workshops/javasamples/javacodechallenges/LinkedinCourseExercisesTest.java
+++ src/test/java/com/workshops/javasamples/javacodechallenges/LinkedinCourseExercisesTest.java
@@ -1,11 +1,19 @@
 package com.workshops.javasamples.javacodechallenges;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedinCourseExercisesTest {
+
+    private final ByteArrayOutputStream gamePrintOut = new ByteArrayOutputStream();
 
     @Test
     void isEven_true() {
@@ -39,5 +47,47 @@ class LinkedinCourseExercisesTest {
     @Test
     void isPasswordComplexRegex_false(){
         assertFalse(LinkedinCourseExercises.isPasswordComplexRegex("123asd"));
+    }
+
+    @BeforeEach
+    public void setupStreams() {
+        System.setOut(new PrintStream(gamePrintOut));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void askUserToContinue_Yes() {
+        String input = "yes";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        LinkedinCourseExercises exercises = new LinkedinCourseExercises();
+        assertTrue(exercises.getIsStillPlaying());
+
+        exercises.askUserToContinue();
+        assertTrue(exercises.getIsStillPlaying());
+        assertEquals("Would you like to continue " +
+                "and try to double your winnings? " +
+                "Enter YES to continue\n", gamePrintOut.toString());
+    }
+
+    @Test
+    public void askUserToContinue_No() {
+        String input = "no";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        LinkedinCourseExercises exercises = new LinkedinCourseExercises();
+        assertTrue(exercises.getIsStillPlaying());
+
+        exercises.askUserToContinue();
+        assertFalse(exercises.getIsStillPlaying());
+        assertEquals("Would you like to continue " +
+                "and try to double your winnings? " +
+                "Enter YES to continue\n", gamePrintOut.toString());
     }
 }
