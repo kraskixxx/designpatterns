d6944802e74a2be735e930e22e99365ed1567bc1
Samet Budak 0291000349 <samet.budak@netaszm.com.tr>
2021-08-27 12:58:54 +0300

    Finding the first non-repeated character

--- src/main/java/com/workshops/javasamples/realworldcoding/StringsNumbersAndMath.java
+++ src/main/java/com/workshops/javasamples/realworldcoding/StringsNumbersAndMath.java
@@ -6,16 +6,18 @@ import java.util.stream.Collectors;
 
 public class StringsNumbersAndMath {
 
+    private static final int EXTENDED_ASCII_CODES = 256;
+
     /**
      * Counting duplicate characters
      */
 
-    public static Map<String, Integer> countDuplicateCharacters(String str){
+    public static Map<String, Integer> countDuplicateCharacters(String str) {
 
         Map<String, Integer> result = new HashMap<>();
 
         for (char ch : str.toCharArray()) {
-            result.compute(String.valueOf(Character.toChars(ch)), (k,v) -> (v==null) ? 1 : ++v);
+            result.compute(String.valueOf(Character.toChars(ch)), (k, v) -> (v == null) ? 1 : ++v);
         }
 
         return result;
@@ -32,7 +34,37 @@ public class StringsNumbersAndMath {
         return result;
     }
 
+    /**
+     * Finding the first non-repeated character
+     */
+    public static char firstNonRepeatedChar(String str) {
+        int[] flags = new int[EXTENDED_ASCII_CODES];
 
+        for (int i = 0; i < flags.length; i++) {
+            flags[i] = -1;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (flags[ch] == -1) {
+                flags[ch] = i;
+            } else {
+                flags[ch] = -2;
+            }
+        }
+
+        int position = Integer.MAX_VALUE;
+
+        for (int i = 0; i < EXTENDED_ASCII_CODES; i++) {
+            if (flags[i] >= 0) {
+                position = Math.min(position, flags[i]);
+            }
+        }
+
+        return position == Integer.MAX_VALUE ? Character.MIN_VALUE : str.charAt(position);
+
+    }
 
 
     public static void main(String[] args) {
