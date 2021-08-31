ada3dcd648c2c513bd6325dbf297220377847614
Samet Budak 0291000349 <samet.budak@netaszm.com.tr>
2021-08-31 15:35:44 +0300

    outer class inner class sample

---
+++ src/main/java/com/workshops/javasamples/grokkingjavainterview/OuterClass.java
@@ -0,0 +1,23 @@
+package com.workshops.javasamples.grokkingjavainterview;
+
+public class OuterClass {
+
+    static int outer_x = 10;
+    int outer_y = 20;
+    private int outer_z = 30;
+
+    class InnerClass {
+        void print() {
+            System.out.println("outer_x = " + outer_x);
+            System.out.println("outer_y = " + outer_y);
+            System.out.println("outer_z = " + outer_z);
+        }
+    }
+
+    public static void main(String[] args) {
+        OuterClass outerClass = new OuterClass();
+        OuterClass.InnerClass innerClass = outerClass.new InnerClass();
+
+        innerClass.print();
+    }
+}
