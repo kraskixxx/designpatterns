13fcf2e486081aab26bcb5423b94b6b98c3dc18a
Samet Budak 0291000349 <samet.budak@netaszm.com.tr>
2021-09-13 12:12:15 +0300

    leetcode examples

---
+++ src/main/java/com/workshops/javasamples/leetcode/ReverseInteger.java
@@ -0,0 +1,12 @@
+package com.workshops.javasamples.leetcode;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        String reversed = new StringBuilder().append(Math.abs(x)).reverse().toString();
+        try {
+            return (x<0) ? Integer.parseInt(reversed) * -1 : Integer.parseInt(reversed);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+}
